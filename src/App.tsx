dab4c4707e0693c594b92481e62330c6cd6ae5bf
Julliane Di Paula <dipaulajulliane@gmail.com>
2025-01-30 16:19:33 -0300

    :tada: Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChangeEvent } from "react";
+import Logo from "./assets/Logo.png"
+import { CiCirclePlus } from "react-icons/ci";
+import { v4 as uuidv4 } from 'uuid';
+
+
+const App = () => {
+  return (
+ <div></div>
+)
+}
+
+export default App;
